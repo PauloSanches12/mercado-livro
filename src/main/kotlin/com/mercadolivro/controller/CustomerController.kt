cdbf8ddb6beb87ede831c852448a6b4dad1f1cd9
Paulo <paulo_sanches2012@hotmail.com>
2022-01-09 10:30:04 -0300

    created method Put

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,6 +1,7 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -17,8 +18,8 @@ class CustomerController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody custumer: PostCustomerRequest){
-        val id = if(customers.isEmpty()) {
+    fun create(@RequestBody custumer: PostCustomerRequest) {
+        val id = if (customers.isEmpty()) {
             1
         } else {
             customers.last().id.toInt() + 1
@@ -29,5 +30,14 @@ class CustomerController {
     @GetMapping("/{id}")
     fun getCostumer(@PathVariable id: String): CustomerModel {
         return customers.first { it.id == id }
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody custumer: PutCustomerRequest) {
+        customers.first { it.id == id }.let {
+            it.name = custumer.name
+            it.email = custumer.email
+        }
     }
 }
