1912e6a03f0263f8daa61974ca42b6ac55241233
Paulo <paulo_sanches2012@hotmail.com>
2022-01-30 11:12:16 -0300

    created methods book

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -1,11 +1,14 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostBookRequest
+import com.mercadolivro.controller.request.PutBookRequest
 import com.mercadolivro.extension.toBookModel
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import java.awt.print.Book
 
 @RestController
 @RequestMapping("book")
@@ -18,5 +21,29 @@ class BookController(
     fun create(@RequestBody request: PostBookRequest) {
         val customer = customerService.getById(request.customerId)
         bookService.create(request.toBookModel(customer))
+    }
+
+    @GetMapping
+    fun findAll(): List<BookModel> = bookService.findAll()
+
+    @GetMapping("/active")
+    fun findActives(): List<BookModel> = bookService.findActives()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): BookModel {
+        return bookService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int) {
+        bookService.delete(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest) {
+        val bookSaved = bookService.findById(id)
+        bookService.update(book.toBookModel(bookSaved))
     }
 }
--- src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -1,8 +1,12 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.repository.BookRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @Service
 class BookService(val bookRepository: BookRepository) {
@@ -10,4 +14,27 @@ class BookService(val bookRepository: BookRepository) {
         bookRepository.save(book)
     }
 
+    fun findAll(): List<BookModel> {
+       return bookRepository.findAll().toList()
+    }
+
+    fun findActives(): List<BookModel> {
+        return bookRepository.findByStatus(BookStatus.ATIVO)
+    }
+
+    fun findById(id: Int): BookModel {
+        return bookRepository.findById(id).orElseThrow()
+    }
+
+    fun delete(id: Int) {
+        val book = findById(id)
+
+        book.status = BookStatus.CANCELADO
+
+        update(book)
+    }
+
+    fun update(book: BookModel) {
+        bookRepository.save(book)
+    }
 }
